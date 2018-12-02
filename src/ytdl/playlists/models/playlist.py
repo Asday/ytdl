1c6624eb23d0780810d1956c6de7858c1e85c809
asday <sara.and.zuka@gmail.com>
2018-12-02 13:34:21 +0000

    Added the ability to create and update `Video`s from a playlist.
    Also of note:  That is a HUGE test.

--- src/ytdl/playlists/models/playlist.py
+++ src/ytdl/playlists/models/playlist.py
@@ -1,8 +1,58 @@
+from django.apps import apps
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
+from .video import Video
 
 
 class Playlist(models.Model):
     added_by = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     youtube_id = models.CharField(max_length=34, unique=True)
+
+    def create_and_update_videos(self):
+        downloader = apps.get_app_config('downloader')
+
+        now = timezone.now()
+        results = {
+            result['id']: result
+            for result in downloader.get_playlist_info(self.youtube_id)
+        }
+
+        for video in self.videos.all():
+            result = results.pop(video.youtube_id, None)
+            if result is None:
+                video.removed = now
+
+                video.save()
+
+                continue
+
+            if result['title'] == video.title:
+                continue
+
+            video.deleted = False
+            video.privated = False
+
+            if result['title'] == '[Private video]':
+                video.privated = True
+            elif result['title'] == '[Deleted video]':
+                video.deleted = True
+            else:
+                video.title = result['title']
+
+            video.save()
+
+        # All that's left in `results` will be new stuff.
+        Video.objects.bulk_create([
+            Video(
+                playlist=self,
+                youtube_id=result['id'],
+                title=result['title'],
+                added=now,
+                deleted=result['title'] == '[Deleted video]',
+                privated=result['title'] == '[Private video]',
+            )
+            for result in results.values()
+        ])
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,15 @@
+import datetime
+
+from django.apps import apps
 from django.core.management import call_command
+
+import attr
+from freezegun import freeze_time
+import pytest
+import pytz
+
+from downloader.exceptions import YoutubeDLError
+from playlists.models import Playlist, Video
 
 
 def test_server_starts(client):
@@ -35,3 +46,284 @@ def test_get_playlist_info_returns_id_and_title_for_all_results():
     for result in results:
         assert 'id' in result
         assert 'title' in result
+
+
+@attr.s
+class Params(object):
+    preexisting = attr.ib()
+    playlist_info = attr.ib()
+    expected = attr.ib()
+
+
+now = datetime.datetime(2018, 12, 2, 0, 0, 0, tzinfo=pytz.UTC)
+yesterday = datetime.datetime(2018, 12, 1, 0, 0, 0, tzinfo=pytz.UTC)
+
+
+@freeze_time('2018-12-02 00:00:00.0')
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'params',
+    [
+        Params(  # None preexisting, none new.
+            preexisting=[],
+            playlist_info=[],
+            expected=[],
+        ),
+        Params(  # None preexisting, one new.
+            preexisting=[],
+            playlist_info=[{'id': 'testID', 'title': 'Test Title'}],
+            expected=[
+                {
+                    'youtube_id': 'testID',
+                    'title': 'Test Title',
+                    'added': now,
+                    'removed': None,
+                },
+            ]
+        ),
+        Params(  # None preexisting, some new.
+            preexisting=[],
+            playlist_info=[
+                {'id': 'testID1', 'title': 'Test Title 1'},
+                {'id': 'testID2', 'title': 'Test Title 2'},
+            ],
+            expected=[
+                {
+                    'youtube_id': 'testID1',
+                    'title': 'Test Title 1',
+                    'added': now,
+                    'removed': None,
+                },
+                {
+                    'youtube_id': 'testID2',
+                    'title': 'Test Title 2',
+                    'added': now,
+                    'removed': None,
+                },
+            ],
+        ),
+        Params(  # Some preexisting, none new.
+            preexisting=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'added': now,
+                'removed': None,
+            }],
+            playlist_info=[{'id': 'testID', 'title': 'Test Title'}],
+            expected=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'added': now,
+                'removed': None,
+            }],
+        ),
+        Params(  # Some preexisting, one new.
+            preexisting=[{
+                'youtube_id': 'testID1',
+                'title': 'Test Title 1',
+                'added': yesterday,
+                'removed': None,
+            }],
+            playlist_info=[
+                {'id': 'testID1', 'title': 'Test Title 1'},
+                {'id': 'testID2', 'title': 'Test Title 2'},
+            ],
+            expected=[
+                {
+                    'youtube_id': 'testID1',
+                    'title': 'Test Title 1',
+                    'added': yesterday,
+                    'removed': None,
+                },
+                {
+                    'youtube_id': 'testID2',
+                    'title': 'Test Title 2',
+                    'added': now,
+                    'removed': None,
+                },
+            ],
+        ),
+        Params(  # Some preexisting, one removed.
+            preexisting=[
+                {
+                    'youtube_id': 'testID1',
+                    'title': 'Test Title 1',
+                    'added': yesterday,
+                    'removed': None,
+                },
+                {
+                    'youtube_id': 'testID2',
+                    'title': 'Test Title 2',
+                    'added': yesterday,
+                    'removed': None,
+                },
+            ],
+            playlist_info=[{'id': 'testID1', 'title': 'Test Title 1'}],
+            expected=[
+                {
+                    'youtube_id': 'testID1',
+                    'title': 'Test Title 1',
+                    'added': yesterday,
+                    'removed': None,
+                },
+                {
+                    'youtube_id': 'testID2',
+                    'title': 'Test Title 2',
+                    'added': yesterday,
+                    'removed': now,
+                },
+            ],
+        ),
+        Params(  # Some preexisting, one new, one removed.
+            preexisting=[
+                {
+                    'youtube_id': 'testID1',
+                    'title': 'Test Title 1',
+                    'added': yesterday,
+                    'removed': None,
+                },
+                {
+                    'youtube_id': 'testID2',
+                    'title': 'Test Title 2',
+                    'added': yesterday,
+                    'removed': None,
+                },
+            ],
+            playlist_info=[
+                {'id': 'testID1', 'title': 'Test Title 1'},
+                {'id': 'testID3', 'title': 'Test Title 3'},
+            ],
+            expected=[
+                {
+                    'youtube_id': 'testID1',
+                    'title': 'Test Title 1',
+                    'added': yesterday,
+                    'removed': None,
+                },
+                {
+                    'youtube_id': 'testID2',
+                    'title': 'Test Title 2',
+                    'added': yesterday,
+                    'removed': now,
+                },
+                {
+                    'youtube_id': 'testID3',
+                    'title': 'Test Title 3',
+                    'added': now,
+                    'removed': None,
+                },
+            ],
+        ),
+        Params(  # Some preexisting, one renamed.
+            preexisting=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'added': yesterday,
+                'removed': None,
+            }],
+            playlist_info=[{'id': 'testID', 'title': 'Renamed'}],
+            expected=[{
+                'youtube_id': 'testID',
+                'title': 'Renamed',
+                'added': yesterday,
+                'removed': None,
+            }],
+        ),
+        Params(  # Some preexisting, one deleted.
+            preexisting=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'added': yesterday,
+                'removed': None,
+                'deleted': False,
+            }],
+            playlist_info=[{'id': 'testID', 'title': '[Deleted video]'}],
+            expected=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'deleted': True,
+                'privated': False,
+            }],
+        ),
+        Params(  # Some preexisting, one made private.
+            preexisting=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'added': yesterday,
+                'removed': None,
+                'privated': False,
+            }],
+            playlist_info=[{'id': 'testID', 'title': '[Private video]'}],
+            expected=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'deleted': False,
+                'privated': True,
+            }],
+        ),
+        Params(  # Some preexisting private, one made public.
+            preexisting=[{
+                'youtube_id': 'testID',
+                'title': '[Private video]',
+                'added': yesterday,
+                'removed': None,
+                'privated': True,
+            }],
+            playlist_info=[{'id': 'testID', 'title': 'Test Title'}],
+            expected=[{
+                'youtube_id': 'testID',
+                'title': 'Test Title',
+                'deleted': False,
+                'privated': False,
+            }],
+        ),
+        Params(  # None preexisting, one new private, one new deleted.
+            preexisting=[],
+            playlist_info=[
+                {'id': 'testID1', 'title': '[Private video]'},
+                {'id': 'testID2', 'title': '[Deleted video]'},
+            ],
+            expected=[
+                {
+                    'youtube_id': 'testID1',
+                    'title': '[Private video]',
+                    'added': now,
+                    'removed': None,
+                    'deleted': False,
+                    'privated': True,
+                },
+                {
+                    'youtube_id': 'testID2',
+                    'title': '[Deleted video]',
+                    'added': now,
+                    'removed': None,
+                    'deleted': True,
+                    'privated': False,
+                },
+            ],
+        ),
+    ],
+)
+def test_create_and_update_videos(params, mocker):
+    playlist = Playlist.objects.create(youtube_id='playlistID')
+
+    for details in params.preexisting:
+        Video.objects.create(playlist=playlist, **details)
+
+    downloader = apps.get_app_config('downloader')
+
+    mocker.patch.object(downloader, 'get_playlist_info')
+    downloader.get_playlist_info.return_value = (
+        item for item in params.playlist_info
+    )
+
+    playlist.create_and_update_videos()
+
+    videos = playlist.videos.all()
+    for details in params.expected:
+        video = videos.get(youtube_id=details['youtube_id'])
+
+        for attr_name, value in details.items():
+            assert getattr(video, attr_name) == value
+
+    assert playlist.videos.count() == len(params.expected)
